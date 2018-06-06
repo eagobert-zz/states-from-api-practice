59e319c0cf17f4d17c877a0a65b71a7b09e51aef
eagobert <eagobert@hotmail.com>
2018-06-05 20:37:42 -0500

    good stopping point

--- src/Person.js
+++ src/Person.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import Vehicle from './Vehicle'
 
 
 class Person extends Component {
@@ -8,6 +9,7 @@ class Person extends Component {
             <h2>{this.props.person.firstName} {this.props.person.lastName}</h2>
             <p>{this.props.person.address.street}</p>
             <p>{this.props.person.address.city}, {this.props.person.address.state}</p>
+            <Vehicle vehicles={this.props.vehicles}/>
             </span>
         )
     }
